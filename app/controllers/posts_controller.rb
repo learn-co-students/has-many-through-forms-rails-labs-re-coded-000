86629c980a26cb863640d9cec55fcc01c3a67820
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2017-01-16 13:12:04 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,29 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.find(params[:id])
-  end
+     @post = Post.find(params[:id])
+     @comment = @post.comments.build
+     @comment.user = User.new
+    #@users = @post.comments.map(&:user) & @post.comments.map(&:user)
+     @users = @post.comments.select("distinct user_id").map {|x| x.user}
+   end
 
-  def index
-    @posts = Post.all
-  end
+   def index
+     @posts = Post.all
+   end
 
-  def new
-    @post = Post.new
-  end
+   def new
+     @post = Post.new
+     @post.categories.build
+   end
 
-  def create
-    post = Post.create(post_params)
-    redirect_to post
-  end
+   def create
+     @post = Post.create(post_params)
+     redirect_to @post
+   end
 
-  private
+   private
 
-  def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
-  end
+   def post_params
+     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+   end
 end
