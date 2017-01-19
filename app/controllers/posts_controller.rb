8bc951ad764870bece4a40bd024ad238d8b8b345
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2017-01-19 05:13:23 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,32 @@
 class PostsController < ApplicationController
   def show
-     @post = Post.find(params[:id])
-     @comment = @post.comments.build
-     @comment.user = User.new
-    #@users = @post.comments.map(&:user) & @post.comments.map(&:user)
-     @users = @post.comments.select("distinct user_id").map {|x| x.user}
-   end
+    @post = Post.find(params[:id])
+    @users = @post.comments.select(DISTINCT user_id).map {|x| x.user}
+    @comment = @post.comments.build
+    @comment.user=User.new
+   #@users = @post.comments.map(&:user) & @post.comments.map(&:user)
+  end
 
-   def index
-     @posts = Post.all
-   end
+  def index
+    @posts = Post.all
+  end
 
-   def new
-     @post = Post.new
-     @post.categories.build
-   end
+  def new
+    @post = Post.new
+    @post.categories.build
 
-   def create
-     @post = Post.create(post_params)
-     redirect_to @post
-   end
 
-   private
+  end
 
-   def post_params
-     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
-   end
+  def create
+
+    post = Post.create(post_params)
+    redirect_to post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+  end
 end
