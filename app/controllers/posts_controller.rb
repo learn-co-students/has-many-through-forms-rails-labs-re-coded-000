b33398ff9d54e5993af0c8a387d08b2ffb56d6ac
AyedhSh <alshmary_ad@yahoo.com>
2017-02-02 21:26:37 +0300

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+   @comment=@post.comments.build
+   @comment.user=User.new
+   @users = User.joins(:comments).includes(:comments).where(:comments => {post_id: @post.id}).distinct
   end
 
   def index
@@ -9,9 +12,13 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build
+
+
   end
 
   def create
+
     post = Post.create(post_params)
     redirect_to post
   end
